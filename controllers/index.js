0081f5c00ca613b874ea61ed85f8e82d64b663bf
haojie <haojie@taotaosou.com>
2017-07-03 17:05:28 +0800

    change

--- controllers/index.js
+++ controllers/index.js
@@ -5,7 +5,7 @@ exports.index = function(req, res, next) {
   //返回指定文件
   //res.sendFile(path.resolve('./public') + '/index.html');
   //渲染页面
-  res.render('index', {title: 'node+express'});
+  res.render('index', {title: 'node+express', ip: req.ip});
   //返回json数据
   //res.json({name: 'haojie', age: '1993'})
 };
